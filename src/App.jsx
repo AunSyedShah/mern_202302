b126eb2ea5fbff442a10014fd4cb2f3b3778a02f
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-05-12 07:12:01 +0000

    apply tailwind class and reuse component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-export default function App(){
-  return(
+import NavBar from "./components/NavBar";
+
+export default function App() {
+  return (
     <div>
+      <NavBar/>
       <h1 className="text-9xl text-red-500">Hello World</h1>
-      <button className="text-yellow-300">Hello World</button>
+      <button className="px-4 py-2 text-yellow-300 border border-amber-400 rounded-full hover:bg-red-400 hover:text-white transition duration-300">
+        Hello World
+      </button>
     </div>
   )
 }
