9f429a013806483a639050f7fbda772954573cfb
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-05-12 07:02:04 +0000

    add a class

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 export default function App(){
   return(
     <div>
-      <h1 className="">Hello World</h1>
+      <h1 className="text-9xl text-red-500">Hello World</h1>
+      <button className="text-yellow-300">Hello World</button>
     </div>
   )
 }
