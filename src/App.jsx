af35ba4c1e794a798236e203e14be24c26091394
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-05-16 06:47:36 +0000

    start of map

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import Button from "./components/Button";
-import NavBar from "./components/NavBar";
-
 export default function App() {
+  let friends = ['hamza', 'humayun', 'ahmed'];
   return (
     <div>
-      <NavBar/>
-      <h1 className="text-9xl text-red-500">Hello World</h1>
-      <Button text="Click Me"/>
-      <Button text="About Us"/>
-      <Button  text="Order Now"/>
+      
     </div>
   )
 }
