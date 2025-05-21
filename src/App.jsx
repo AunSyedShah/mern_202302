e45b454d5a75bbb7dd7140237c24477ef41dd1ab
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-05-21 06:58:08 +0000

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from "axios"
+
 export default function App() {
-  let friends = ['hamza', 'humayun', 'ahmed'];
+
+  let [data, setData] = useState({});
+
+  async function get_data() {
+    let response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+    console.log(response.data)
+    setData(response.data)
+  }
+
+
+  useEffect(function handler(){
+    get_data()
+  }, [])
+
   return (
     <div>
-      
+      <h1 className="text-5xl">{data.title}</h1>
+
     </div>
   )
 }
