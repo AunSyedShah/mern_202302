8d7c552839c6a4f1267aa261475d808d3ec3b3ea
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-05-23 06:49:37 +0000

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import { useState, useEffect } from "react";
-import axios from "axios"
+import { useState } from "react"
 
-export default function App() {
+export default function App(){
+  let [fruits, setFruits] = useState([
+    {
+      "fruitName": "Mango",
+      "price": 250
+    },
+    {
+      "fruitName": "Banana",
+      "price": 100
+    },
+    {
+      "fruitName": "Cucumber",
+      "price": 80
+    },
+  ]);
 
-  let [data, setData] = useState([]);
-
-  async function get_data() {
-    let response = await axios.get("https://jsonplaceholder.typicode.com/todos");
-    console.log(response.data)
-    setData(response.data)
-  }
-
-
-  useEffect(function handler(){
-    get_data()
-  }, [])
-
-  return (
+  return(
     <div>
-      {/* <h1 className="text-5xl">{data.title}</h1> */}
-
+      <h1>Hello World</h1>
+      {
+        fruits.map((fruit) => {
+          return <h1 className="text-5xl">{fruit.fruitName}</h1>
+        })
+      }
     </div>
   )
 }
