d38932438884382077fd3eef9cdd419299686fa9
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-05-23 07:28:21 +0000

    api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
+import axios from "axios";
 import { useState } from "react"
 
 export default function App(){
-  let [fruits, setFruits] = useState([
-    {
-      "fruitName": "Mango",
-      "price": 250
-    },
-    {
-      "fruitName": "Banana",
-      "price": 100
-    },
-    {
-      "fruitName": "Cucumber",
-      "price": 80
-    },
-  ]);
+  let [fruits, setFruits] = useState([]);
+
+  async function get_data(){
+    let response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    setFruits(response.data);
+  }
 
   return(
     <div>
       <h1>Hello World</h1>
       {
-        fruits.map((fruit) => {
-          return <h1 className="text-5xl">{fruit.fruitName}</h1>
-        })
+        fruits && fruits.length > 0 ? fruits.map((fruit) => {
+          return(
+            <div className="bg-gray-200 p-2 w-min m-2">
+              <h1>{fruit.title}</h1>
+              <h1>{fruit.id}</h1>
+            </div>
+          )
+        }) : <h1 className="text-3xl text-red-400">No Data Found</h1>
       }
+
+      <button className="bg-green-400 text-white rounded-2xl p-2" onClick={get_data}>Get Data</button>
     </div>
   )
 }
