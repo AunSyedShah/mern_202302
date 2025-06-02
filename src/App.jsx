58f9a485bcd002008e50352708ad9038580bfc60
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-06-02 06:58:13 +0000

    formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-import axios from "axios";
-import { useState } from "react"
+import { useFormik } from "formik"
 
 export default function App(){
-  let [fruits, setFruits] = useState([]);
-
-  async function get_data(){
-    let response = await axios.get("https://jsonplaceholder.typicode.com/todos");
-    setFruits(response.data);
-  }
-
+  let formik = useFormik(
+    {
+      initialValues: {
+        username: '',
+        password: ''
+      },
+      onSubmit: (values) => {
+        console.log(values);
+      }
+    }
+  );
   return(
     <div>
-      <h1>Hello World</h1>
-      {
-        fruits && fruits.length > 0 ? fruits.map((fruit) => {
-          return(
-            <div className="bg-gray-200 p-2 w-min m-2">
-              <h1>{fruit.title}</h1>
-              <h1>{fruit.id}</h1>
-            </div>
-          )
-        }) : <h1 className="text-3xl text-red-400">No Data Found</h1>
-      }
-
-      <button className="bg-green-400 text-white rounded-2xl p-2" onClick={get_data}>Get Data</button>
+      <form onSubmit={formik.handleSubmit}>
+        <input type="text" name="username" value={formik.values.username} onChange={formik.handleChange}/>
+        <input type="password" name="password" value={formik.values.password} onChange={formik.handleChange}/>
+        <input type="submit"/>
+      </form>
     </div>
   )
 }
